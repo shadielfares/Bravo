cab4c54e32bd36adf1d46e88198b8a0cf1b31862
ahmed-z5645 <ahmed.zafar5645@gmail.com>
2024-11-10 11:15:32 -0500

    created react app RAHHHHH

---
+++ front-end/mec/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Taskbar">
+      </div>
+      <div className="Content">
+        <div className="Welcome">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
